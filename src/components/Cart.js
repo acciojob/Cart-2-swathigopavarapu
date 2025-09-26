88c5274566a1a230488a2dbb5689b24adef53750
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-26 21:02:08 +0530

    initial commit : Cart2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,33 +12,13 @@ const Cart = () => {
         {state.cart.map(item => (
           <li key={item.id}>
             {item.name} - ${item.price} x {item.quantity}
-
-            <button
-              id={`increment-btn-${item.id}`}
-              onClick={() => dispatch({ type: "INCREMENT", payload: item.id })}
-            >
-              +
-            </button>
-
-            <button
-              id={`decrement-btn-${item.id}`}
-              onClick={() => dispatch({ type: "DECREMENT", payload: item.id })}
-            >
-              -
-            </button>
-
-            <button
-              id={`cart-item-remove-${item.id}`}
-              onClick={() => dispatch({ type: "REMOVE_ITEM", payload: item.id })}
-            >
-              Remove
-            </button>
+            <button onClick={() => dispatch({ type: "INCREMENT", payload: item.id })}>+</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", payload: item.id })}>-</button>
+            <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: item.id })}>Remove</button>
           </li>
         ))}
       </ul>
-
       <p id="cart-total-amount">Total: ${totalAmount}</p>
-
       <button id="clear-all-cart" onClick={() => dispatch({ type: "CLEAR_CART" })}>
         Clear Cart
       </button>
